f69d45547a510ae2ec74e5b4d77e6f9df6a1788f
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2024-07-22 17:33:16 +0300

    First Commit

---
+++ app/(routes)/layout.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+interface RoutesLayoutProps{
+    children: React.ReactNode
+}
+
+const RoutesLayout = ({children}:RoutesLayoutProps) => {
+  return (
+
+    <>
+    Header
+
+    {children}
+
+    Footer
+    
+    </>
+    
+  )
+}
+
+export default RoutesLayout
