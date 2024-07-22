13d9d77814c6d8e602977b4d96090fa8228e2006
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2024-07-22 20:53:25 +0300

    Nextjs Dersleri 6. Gün | Proje 2 E-Ticaret Sitesi | 6. Gün | Kurulum, Ayarlar, Dark Mode ve Shadcn Giriş

--- app/(routes)/(home)/page.tsx
+++ app/(routes)/(home)/page.tsx
@@ -1,9 +1,39 @@
+import { ModeToggle } from "@/components/ModeToggle";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { MenuIcon } from "lucide-react";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-      Home
+      <ModeToggle/>
+      <Button variant="greenBtn" size="icon">deneme</Button>
+      <button>test</button>
+
+      <div className="mt-32">
+
+      <DropdownMenu>
+  <DropdownMenuTrigger><MenuIcon/></DropdownMenuTrigger>
+  <DropdownMenuContent>
+    <DropdownMenuLabel>My Account</DropdownMenuLabel>
+    <DropdownMenuSeparator />
+    <DropdownMenuItem>Profile</DropdownMenuItem>
+    <DropdownMenuItem>Billing</DropdownMenuItem>
+    <DropdownMenuItem>Team</DropdownMenuItem>
+    <DropdownMenuItem>Subscription</DropdownMenuItem>
+  </DropdownMenuContent>
+</DropdownMenu>
+
+</div>
     </div>
   );
 }
