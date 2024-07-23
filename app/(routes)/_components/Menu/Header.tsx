d999a2cee58ba28e940edac41097d4e9af29fc98
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2024-07-23 21:33:16 +0300

    Link usePathname

--- app/(routes)/_components/Menu/Header.tsx
+++ app/(routes)/_components/Menu/Header.tsx
@@ -7,39 +7,46 @@ import NavMenu from './NavMenu'
 import { Button } from '@/components/ui/button'
 import MobileMenu from './MobileMenu'
 import CartMenu from './Cart'
+import Link from 'next/link'
 
-const pacifiko = Pacifico({subsets:["cyrillic"], weight:"400"})
+const pacifiko = Pacifico({ subsets: ["cyrillic"], weight: "400" })
 
 const Header = () => {
-  return (
-    <div className='mx-auto bgone shadow-md'>
-        <div className='container flex flex-row items-center
+    return (
+        <div className='mx-auto bgone shadow-md'>
+            <div className='container flex flex-row items-center
          justify-between p-5'>
-            <div>
-                <h2 className={`${pacifiko.className} text-xl`}>ECommerce</h2>
-            </div>
-            <div className='hidden md:flex relative md:min-w-96 lg:w-1/2'>
-                <Input className='w-full border-2'/>
-                <Button variant="link" className='absolute right-1 '>
-                    <Search/>
-                </Button>
+                <div>
+                    <h2 className={`${pacifiko.className} text-xl`}>ECommerce</h2>
+                </div>
+                <div className='hidden md:flex relative md:min-w-96 lg:w-1/2'>
+                    <Input className='w-full border-2' />
+                    <Button variant="link" className='absolute right-1 '>
+                        <Search />
+                    </Button>
 
 
-            </div>
-            <div className='flex flex-row items-center space-x-3'>
-            <ModeToggle/>
-               <Heart/>
-              <CartMenu/>
-               <UserIcon/>
-             
-               <MobileMenu/>
+                </div>
+                <div className='flex flex-row items-center space-x-3'>
+                    <ModeToggle />
+                    <Link href="/fav">
+                        <Heart />
+                    </Link>
+
+                    <CartMenu />
+                    <Link href="/login">
+                        <UserIcon />
+                    </Link>
+
+
+                    <MobileMenu />
+                </div>
+
             </div>
 
+            <NavMenu />
         </div>
-
-        <NavMenu/>
-    </div>
-  )
+    )
 }
 
 export default Header
--- app/(routes)/_components/Menu/NavMenu.tsx
+++ app/(routes)/_components/Menu/NavMenu.tsx
@@ -14,75 +14,96 @@ import {
 } from "@/components/ui/navigation-menu"
 import Link from 'next/link'
 import { categories } from '@/constans'
+import { usePathname } from 'next/navigation'
 
 
 
 
 
-  
+
 const NavMenu = () => {
+
+  const pathname = usePathname();
+
+  console.log(pathname)
   return (
     <div className='flex justify-center items-center py-2 bg-mycolor-300'>
-  <NavigationMenu>
-      <NavigationMenuList>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger className='bgHeader'>Recent Product</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-              <li className="row-span-3">
-                <NavigationMenuLink asChild>
-                  <a
-                    className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
-                    href="/"
-                  >
-                    <div className="mb-2 mt-4 text-lg font-medium">
-                      shadcn/ui
-                    </div>
-                    <p className="text-sm leading-tight text-muted-foreground">
-                      Beautifully designed components that you can copy and
-                      paste into your apps. Accessible. Customizable. Open
-                      Source.
-                    </p>
-                  </a>
-                </NavigationMenuLink>
-              </li>
-              <ListItem href="/docs" title="Introduction">
-                Re-usable components built using Radix UI and Tailwind CSS.
-              </ListItem>
-              <ListItem href="/docs/installation" title="Installation">
-                How to install dependencies and structure your app.
-              </ListItem>
-              <ListItem href="/docs/primitives/typography" title="Typography">
-                Styles for headings, paragraphs, lists...etc
-              </ListItem>
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger className='bgHeader'>Categories</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-              {categories.map((component) => (
-                <ListItem
-                  key={component.title}
-                  title={component.title}
-                  href={component.href}
-                >
-                  {component.description}
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <Link href="/" legacyBehavior passHref >
+              <NavigationMenuLink className={`${navigationMenuTriggerStyle()} bgHeader ${pathname === "/" ? "activeHeader" : ""} `}>
+                Home
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+
+          <NavigationMenuItem>
+            <Link href="/about" legacyBehavior passHref>
+              <NavigationMenuLink className={`${navigationMenuTriggerStyle()} bgHeader ${pathname === "/about" ? "activeHeader" : ""} `}>
+                About us
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+
+          <NavigationMenuItem>
+            <NavigationMenuTrigger className='bgHeader'>Recent Product</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+                <li className="row-span-3">
+                  <NavigationMenuLink asChild>
+                    <a
+                      className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                      href="/"
+                    >
+                      <div className="mb-2 mt-4 text-lg font-medium">
+                        shadcn/ui
+                      </div>
+                      <p className="text-sm leading-tight text-muted-foreground">
+                        Beautifully designed components that you can copy and
+                        paste into your apps. Accessible. Customizable. Open
+                        Source.
+                      </p>
+                    </a>
+                  </NavigationMenuLink>
+                </li>
+                <ListItem href="/docs" title="Introduction">
+                  Re-usable components built using Radix UI and Tailwind CSS.
                 </ListItem>
-              ))}
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <Link href="/docs" legacyBehavior passHref>
-            <NavigationMenuLink className={`${navigationMenuTriggerStyle()} bgHeader`}>
-              Documentation
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-      </NavigationMenuList>
-    </NavigationMenu>
+                <ListItem href="/docs/installation" title="Installation">
+                  How to install dependencies and structure your app.
+                </ListItem>
+                <ListItem href="/docs/primitives/typography" title="Typography">
+                  Styles for headings, paragraphs, lists...etc
+                </ListItem>
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger className={`bgHeader ${pathname.startsWith(`/shop`) ? "activeHeader" : ""} `}>Categories</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+                {categories.map((component) => (
+                  <ListItem
+                    key={component.title}
+                    title={component.title}
+                    href={component.href}
+                  >
+                    {component.description}
+                  </ListItem>
+                ))}
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href="/contact" legacyBehavior passHref>
+            <NavigationMenuLink className={`${navigationMenuTriggerStyle()} bgHeader ${pathname==="/contact" ? "activeHeader" : "" } `}>
+                Contact
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
     </div>
   )
 }
