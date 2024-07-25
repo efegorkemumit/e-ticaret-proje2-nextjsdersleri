2ce9e475c0dfb4bc089aaecbc38c88ead7006c75
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2024-07-25 16:18:42 +0300

    Seo

--- app/(routes)/about/page.tsx
+++ app/(routes)/about/page.tsx
@@ -1,7 +1,18 @@
 import React from 'react'
 import { columns } from '../_components/Datatable/columns'
 import { DataTable } from '../_components/Datatable/data-table'
+import { Metadata } from 'next'
 
+
+export const metadata:Metadata={
+  title:"About | Hakkımda ",
+  description:"Efe Görkem Ümit hakkımda",
+  robots:{
+    index:false,
+    follow:false,
+  },
+ 
+}
 
 async function getData(): Promise<Payment[]> {
   // Fetch data from your API here.
@@ -26,10 +37,26 @@ async function getData(): Promise<Payment[]> {
 const AboutPage = async() => {
   const data = await getData()
 
+  const jsonLd = {
+    '@context': 'https://schema.org',
+    '@type': 'Product',
+    author: "efegorkemumit",
+    isPublished: true,
+    tags: ["web development", "nextjs", "mobile development", "shopify development"]
+  }
+
   return (
+    <>
+    <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+      />
+    
+   
     <div className="container mx-auto py-10 border mt-10 rounded-xl dark:border-slate-600">
     <DataTable columns={columns} data={data} />
   </div>
+  </>
   )
 }
 
--- app/(routes)/shop/[slug]/page.tsx
+++ app/(routes)/shop/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import { categories } from '@/constans'
 import React from 'react'
 
 interface ShopDetailPageProps{
@@ -6,9 +7,62 @@ interface ShopDetailPageProps{
     }
 }
 
+export async function generateMetadata({ params }: ProjectPageDetailProps) {
+  const project = categories.find(product => product.href.includes(params.slug))
+
+  if (!project) {
+      return <div>project not found</div>
+  }
+
+  return {
+      title: project.title,
+      description: project.description,
+  }
+}
+
 const ShopDetailPage = ({params}:ShopDetailPageProps) => {
+
+  const project = categories.find(product => product.href.includes(params.slug))
+
+    if (!project) {
+        return <div>project not found</div>
+    }
+
+    const jsonLd = {
+        '@context': 'https://schema.org',
+        '@type': 'Product',
+        'name': project.title,
+        'image': project.image,
+        'description': project.description,
+        'datePublished': project.publishedAt,
+        'dateModified': project.updatedAt,
+        'author': {
+            '@type': 'Person',
+            'name': project.author
+        },
+        'offers': {
+            '@type': 'Offer',
+            'url': project.href,
+            'price': '0.00',
+            'priceCurrency': 'USD',
+            'itemCondition': 'https://schema.org/NewCondition',
+            'availability': 'https://schema.org/InStock'
+        },
+        'aggregateRating': {
+            '@type': 'AggregateRating',
+            'ratingValue': '4.8',
+            'reviewCount': '120'
+        },
+    };
+
   return (
-    <div>{params.slug}</div>
+    <div>
+         <script
+                type="application/ld+json"
+                dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+            />
+      
+      {params.slug}</div>
   )
 }
 
